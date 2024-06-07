096b706de6f426dbc885f43cbac0ad8d0b068f21
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-06-07 07:28:07 +0530

    Created New Project

---
+++ DEVOTE/DEVOTEApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEVOTEApp.swift
+//  DEVOTE
+//
+//  Created by vinay Kumar ranjan on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DEVOTEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
