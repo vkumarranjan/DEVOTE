9568780056c01249b86e0bb490d1320fcc49f238
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-07-02 08:26:31 +0530

    Added card

--- DEVOTE/ContentView.swift
+++ DEVOTE/ContentView.swift
@@ -6,17 +6,71 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    
+    // FETCHING DATA
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                }
+                .onDelete(perform: deleteItems)
+            }
+            
+            .toolbar {
+#if os(iOS)
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+#endif
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                        
+                    }
+                }
+            }
         }
-        .padding()
+        
     }
+    func addItem() {
+        withAnimation{
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+            
+            do {
+                try viewContext.save()
+            } catch {
+                
+            }
+        }
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+      withAnimation {
+        offsets.map { items[$0] }.forEach(viewContext.delete)
+        
+        do {
+          try viewContext.save()
+        } catch {
+          let nsError = error as NSError
+          fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+      }
+    }
+    
+    
 }
 
 #Preview {
