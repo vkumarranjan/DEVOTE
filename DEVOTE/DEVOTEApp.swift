7f43b0c4a964a3bb203008375d61f5e6834067a7
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-08-03 04:43:21 +0530

    Added app file

--- DEVOTE/DEVOTEApp.swift
+++ DEVOTE/DEVOTEApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct DEVOTEApp: App {
+    
+    let persistenceController = PersistenceController.shared
+    
+    @AppStorage("isDarkMode") var isDarkMode: Bool = false
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }
 }
